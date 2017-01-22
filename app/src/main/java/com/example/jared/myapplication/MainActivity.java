59b95f1719f02f40a4f40f44f7ca1db0353f2959
jcgaskin <jcgaskin@comcast.net>
2017-01-22 13:50:50 -0800

    disabled audio disturbances, and looped the screen lock while app is open

--- app/src/main/java/com/example/jared/myapplication/MainActivity.java
+++ app/src/main/java/com/example/jared/myapplication/MainActivity.java
@@ -12,8 +12,6 @@ import android.content.Context;
 
 public class MainActivity extends AppCompatActivity {
 
-    //Content resolver used as a handle to the system's settings
-    private ContentResolver cResolver;
     private AudioManager audioManager = null;
 
 
@@ -31,30 +29,22 @@ public class MainActivity extends AppCompatActivity {
     public void onClickButt(View V){
 
 
+
+
+        //Setting all possible audio disturbances to silent
+        audioManager = (AudioManager)getSystemService(Context.AUDIO_SERVICE);
+        audioManager.setRingerMode(AudioManager.RINGER_MODE_SILENT);
+        audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, 0, 0);
+        audioManager.setStreamVolume(AudioManager.STREAM_ALARM, 0, 0);
+        audioManager.setStreamVolume(AudioManager.STREAM_NOTIFICATION, 0, 0);
+        audioManager.setStreamVolume(AudioManager.STREAM_RING, 0, 0);
+
+        //This locks the device...continually while the app is open
+
         DevicePolicyManager mDevicePolicyManager = (DevicePolicyManager) getSystemService(Context.DEVICE_POLICY_SERVICE);
+        while(1 == 1) {
 
-        mDevicePolicyManager.lockNow();
-
-        //Each of the following "disturbance eliminations" can be moved into it's own method to be cleaner"
-
-
-        //Turning the brightness to 0, need a better way to get rid of visual distractions (this only dims the screen)
-        //Airplane mode cannot be changed without a rooted device
-        //Noifications cannot be stopped without a rooted device
-        //There does seem to be a way to "lock" the phone that only requires admin access
-        //^but this requires permissions that would make most users uncomfortable.
-        //cResolver =  getContentResolver();
-        //Settings.System.putInt(cResolver,Settings.System.SCREEN_BRIGHTNESS_MODE,Settings.System.SCREEN_BRIGHTNESS_MODE_MANUAL);
-        //Settings.System.putInt(cResolver, Settings.System.SCREEN_BRIGHTNESS, 0);
-
-
-
-        //adding a test change
-        //Setting all possible audio disturbances to silentaudioManager.setRingerMode(AudioManager.RINGER_MODE_SILENT);
-        //audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
-        //audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, 0, 0);
-        //audioManager.setStreamVolume(AudioManager.STREAM_ALARM, 0, 0);
-        //audioManager.setStreamVolume(AudioManager.STREAM_NOTIFICATION, 0, 0);
-        //audioManager.setStreamVolume(AudioManager.STREAM_RING, 0, 0);
+            mDevicePolicyManager.lockNow();
+        }
     }
 }
