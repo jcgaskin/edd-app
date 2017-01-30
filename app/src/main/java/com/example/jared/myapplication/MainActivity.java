0c8eccbc10d20ca200a774eecc403d7ff0d06254
jcgaskin <jcgaskin@comcast.net>
2017-01-30 14:27:41 -0800

    added screen that asks for admin

--- app/src/main/java/com/example/jared/myapplication/MainActivity.java
+++ app/src/main/java/com/example/jared/myapplication/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.jared.myapplication;
 
+import android.app.admin.DeviceAdminReceiver;
 import android.app.admin.DevicePolicyManager;
+import android.content.ComponentName;
 import android.content.Intent;
+import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.*;
@@ -13,9 +16,13 @@ import android.content.Context;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int REQUEST_CODE_ENABLE_ADMIN = 117;
     private AudioManager audioManager = null;
-    private SpeedReceiver speedReceiver;
 
+
+    private SpeedReceiver speedReceiver;
+    private DevicePolicyManager mDevicePolicyManager;
+    private DeviceAdminReceiver mDeviceAdminReceiver;
 
 
     @Override
@@ -23,13 +30,40 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        DevicePolicyManager mDevicePolicyManager = (DevicePolicyManager) getSystemService(Context.DEVICE_POLICY_SERVICE);
+        mDevicePolicyManager = (DevicePolicyManager) getSystemService(Context.DEVICE_POLICY_SERVICE);
+        mDeviceAdminReceiver = new DeviceAdminSample();
+
 
         //Creating an instance of SpeedReceiver and starting the StayOnService Service:
         //(These contain the functionality of the app)
         speedReceiver = new SpeedReceiver(mDevicePolicyManager);
         startService(new Intent(this, StayOnService.class));
 
+        askForAdmin();
+
+
+    }
+
+
+    private void askForAdmin(){
+
+        //Every time the user opens the app, we will make a check to determine if the user has the administration
+        //Settings enabled (which allow the app to do its job) if they do not, we will open a UI for them to enable these
+        //settings, and otherwise...?
+
+        ComponentName admin = new ComponentName("com.example.jared.myapplication","DeviceAdminSample");
+
+        if(!mDevicePolicyManager.isAdminActive(admin))
+        {
+            Intent intent = new Intent(DevicePolicyManager.ACTION_ADD_DEVICE_ADMIN);
+            intent.putExtra(DevicePolicyManager.EXTRA_DEVICE_ADMIN, mDeviceAdminReceiver.getWho(this));
+            //intent.putExtra(DevicePolicyManager.EXTRA_ADD_EXPLANATION,
+            //  MainActivity.getString(R.string.add_admin_extra_app_text));
+            startActivityForResult(intent, REQUEST_CODE_ENABLE_ADMIN);
+        }
+
+        //Need to make this function return a value, and basically kill the app if the user
+        //decline the adminstration (as the app is useless without it)
     }
 
     //*****************************THE CODE BELOW IS FOR TESTING ONLY***************************************
--- app/src/main/java/com/example/jared/myapplication/SpeedReceiver.java
+++ app/src/main/java/com/example/jared/myapplication/SpeedReceiver.java
@@ -53,6 +53,5 @@ public class SpeedReceiver extends BroadcastReceiver {
                 }
             }
         }
-
     }
 }
