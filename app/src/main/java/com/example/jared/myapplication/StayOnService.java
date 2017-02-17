0b40717b9c1a7f2a446a917aebea068875d8df0b
jcgaskin <jcgaskin@comcast.net>
2017-02-17 15:26:59 -0800

    Removed testing button

--- app/src/main/java/com/example/jared/myapplication/StayOnService.java
+++ app/src/main/java/com/example/jared/myapplication/StayOnService.java
@@ -46,7 +46,7 @@ public class StayOnService extends Service {
 
         //Requesting updates on the device's location every 10000 milliseconds (every minute)
         //--listener is the callback function
-        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 10000, 0, listener);
+        //locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 10000, 0, listener);
         locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 10000, 0, listener);
         return super.onStartCommand(intent, flags, startId);
     }
